8f5fffa92df879733841bf778fdf41f8a09d8206
Bley-oz <111856046+Bley-oz@users.noreply.github.com>
2022-11-22 18:01:30 -0600

    Add files via upload

---
+++ Curso-Javascript/Ejercicio_02/index.js
@@ -0,0 +1,14 @@
+const lista = [
+    'Bley',
+    32,
+    true,
+    new Date(1989, 6, 10),
+    {
+        titulo: 'El nombre del viento',
+        autor: 'Patrick Rothfuss',
+        fecha: 'marzo 27 2007',
+        url: 'http://lahistoriadekvothe.com/'
+    }
+];
+
+console.log(lista);
