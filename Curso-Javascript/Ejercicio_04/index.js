e94948a3b2597df843f193a284396aa977fe0fca
Bley-oz <111856046+Bley-oz@users.noreply.github.com>
2022-11-23 21:57:02 -0600

    Add files via upload

---
+++ Curso-Javascript/Ejercicio_04/index.js
@@ -0,0 +1,17 @@
+let nombre = 'Bley';
+let apellido = 'Marin';
+
+let estudiante = nombre + " " + apellido;
+
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+
+let varLen = estudiante.length;
+
+let varNombre = estudiante[0];
+
+let varApellido = estudiante[9];
+
+let varEspacios = estudiante.replace(/ /g, '');
+
+let varBooleana = estudiante.includes("Bley");
