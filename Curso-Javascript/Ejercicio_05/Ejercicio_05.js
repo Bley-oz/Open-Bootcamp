cab6b1c37a432a351ab29820a414ccfe019eb6a2
Bley-oz <111856046+Bley-oz@users.noreply.github.com>
2022-11-26 00:43:46 -0600

    Add files via upload

---
+++ Curso-Javascript/Ejercicio_05/Ejercicio_05.js
@@ -0,0 +1,9 @@
+let alturaCm = 178;
+let alturaMt = 1.78;
+let pesoKilo = 65.5;
+
+let alturaRedo = Math.ceil(alturaMt);
+let alturaRedo2 = Math.floor(pesoKilo);
+
+let igualMax = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
